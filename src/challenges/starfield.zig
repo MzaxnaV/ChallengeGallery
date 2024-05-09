8adc3791ba42d1481965a2e588d36cb5f6ff05cf
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-05-09 01:20:14 +0100

    refactor camera

--- src/challenges/starfield.zig
+++ src/challenges/starfield.zig
@@ -1,55 +1,50 @@
 const rl = struct {
     usingnamespace @import("raylib");
+    usingnamespace @import("raylib-math");
 };
 
 const utils = @import("utils");
 const bufPrint = @import("std").fmt.bufPrint;
 
-const star = struct {
-    x: f32 = 0,
-    y: f32 = 0,
-    z: f32 = 0,
+const camera = struct {
+    p: rl.Vector3 = rl.Vector3.init(0, 0, 0),
+    fov: f32,
+};
 
+const star = struct {
+    p: rl.Vector3 = rl.Vector3.init(0, 0, 0),
     pz: f32 = 0,
 
     fn update(self: *star, speed: f32, comptime width: comptime_int, comptime height: comptime_int) void {
-        self.pz = self.z;
-        self.z -= speed;
-        if (self.z <= 1) {
-            self.x = utils.randomFloat(-width, width);
-            self.y = utils.randomFloat(-height, height);
-            self.z = utils.randomFloat(1, width);
+        self.pz = self.p.z;
+        self.p.z -= speed;
+        if (self.p.z <= 1) {
+            self.p.x = utils.randomFloat(-width, width);
+            self.p.y = utils.randomFloat(-height, height);
+            self.p.z = utils.randomFloat(1, width);
 
-            self.pz = self.z;
+            self.pz = self.p.z;
         }
     }
 
-    fn draw(self: @This(), cameraX: f32, cameraY: f32, cameraZ: f32, fov: f32, comptime width: comptime_int, comptime height: comptime_int) void {
-        const relativeX = self.x - cameraX;
-        const relativeY = self.y - cameraY;
-        const relativeZ = self.z - cameraZ;
+    fn draw(self: @This(), cam: camera, comptime width: comptime_int, comptime height: comptime_int) void {
+        const relative = rl.vector3Subtract(self.p, cam.p);
 
-        const scaleFactor = fov / (fov + relativeZ);
+        const scaleFactor = cam.fov / (cam.fov + relative.z);
 
-        // const sx: i32 = @intFromFloat(utils.map((self.x - width / 2) / self.z, 0, 1, 0, width) + width / 2);
-        // const sy: i32 = @intFromFloat(utils.map((self.y - height / 2) / self.z, 0, 1, 0, height) + height / 2);
+        const sx: i32 = @intFromFloat(width / 2 + relative.x * scaleFactor);
+        const sy: i32 = @intFromFloat(height / 2 + relative.y * scaleFactor);
 
-        const sx: i32 = @intFromFloat(width / 2 + relativeX * scaleFactor);
-        const sy: i32 = @intFromFloat(height / 2 + relativeY * scaleFactor);
+        const relativePZ = self.pz - cam.p.z;
+        const scaleFactorP = cam.fov / (cam.fov + relativePZ);
 
-        const relativePZ = self.pz - cameraZ;
-        const scaleFactorP = fov / (fov + relativePZ);
+        const pSX: i32 = @intFromFloat(width / 2 + relative.x * scaleFactorP);
+        const pSY: i32 = @intFromFloat(height / 2 + relative.y * scaleFactorP);
 
-        // const px: i32 = @intFromFloat(utils.map((self.x - width / 2) / self.pz, 0, 1, 0, width) + width / 2);
-        // const py: i32 = @intFromFloat(utils.map((self.y - height / 2) / self.pz, 0, 1, 0, height) + height / 2);
-
-        const px: i32 = @intFromFloat(width / 2 + relativeX * scaleFactorP);
-        const py: i32 = @intFromFloat(height / 2 + relativeY * scaleFactorP);
-
-        const r = utils.map(self.z, 1, width, 16, 0);
+        const r = utils.map(self.p.z, 1, width, 16, 0);
 
         rl.drawCircle(sx, sy, r, rl.Color.white);
-        rl.drawLine(px, py, sx, sy, rl.Color.white);
+        rl.drawLine(pSX, pSY, sx, sy, rl.Color.white);
     }
 };
 
@@ -62,14 +57,16 @@ pub fn run(comptime width: comptime_int, comptime height: comptime_int, comptime
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
 
     for (stars[0..]) |*s| {
-        s.x = utils.randomFloat(-width, width);
-        s.y = utils.randomFloat(-height, height);
-        s.z = utils.randomFloat(1, width);
+        s.p.x = utils.randomFloat(-width, width);
+        s.p.y = utils.randomFloat(-height, height);
+        s.p.z = utils.randomFloat(1, width);
 
-        s.pz = s.z;
+        s.pz = s.p.z;
     }
 
     var text_buffer: [100]u8 = [1]u8{0} ** 100;
+
+    const cam: camera = .{ .fov = 120 };
 
     //--------------------------------------------------------------------------------------
 
@@ -83,13 +80,14 @@ pub fn run(comptime width: comptime_int, comptime height: comptime_int, comptime
         const speed = utils.map(mouse.x, 0, width, 0, 50);
 
         const formatted_text = try bufPrint(text_buffer[0..], "Speed: {d:.2}", .{speed});
-        text_buffer[formatted_text.len] = 0;
+        text_buffer[formatted_text.len] = 0; // Set sentinel value
 
         for (stars[0..]) |*s| {
             s.update(speed, width, height);
         }
 
         //----------------------------------------------------------------------------------
+
         // Draw
         //----------------------------------------------------------------------------------
 
@@ -101,13 +99,7 @@ pub fn run(comptime width: comptime_int, comptime height: comptime_int, comptime
         rl.drawText(text_buffer[0..formatted_text.len :0], 20, 20, 20, rl.Color.gold);
 
         for (stars) |s| {
-            // Simulate camera at position (0, 0, 0) with a field of view of 100
-            const cameraX: f32 = 0;
-            const cameraY: f32 = 0;
-            const cameraZ: f32 = 0;
-            const fov: f32 = 120;
-
-            s.draw(cameraX, cameraY, cameraZ, fov, width, height);
+            s.draw(cam, width, height);
         }
 
         //----------------------------------------------------------------------------------
