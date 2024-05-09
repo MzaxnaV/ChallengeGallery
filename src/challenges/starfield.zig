cf6f27120d15a024965b28fccb447f50ecf3e36d
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-05-09 01:04:46 +0100

    add perspective camera

--- src/challenges/starfield.zig
+++ src/challenges/starfield.zig
@@ -13,6 +13,7 @@ const star = struct {
     pz: f32 = 0,
 
     fn update(self: *star, speed: f32, comptime width: comptime_int, comptime height: comptime_int) void {
+        self.pz = self.z;
         self.z -= speed;
         if (self.z <= 1) {
             self.x = utils.randomFloat(-width, width);
@@ -23,14 +24,27 @@ const star = struct {
         }
     }
 
-    fn draw(self: @This(), comptime width: comptime_int, comptime height: comptime_int) void {
-        const sx: i32 = @intFromFloat(utils.map((self.x - width / 2) / self.z, 0, 1, 0, width) + width / 2);
-        const sy: i32 = @intFromFloat(utils.map((self.y - height / 2) / self.z, 0, 1, 0, height) + height / 2);
+    fn draw(self: @This(), cameraX: f32, cameraY: f32, fov: f32, comptime width: comptime_int, comptime height: comptime_int) void {
+        const relativeX = self.x - cameraX;
+        const relativeY = self.y - cameraY;
 
-        const px: i32 = @intFromFloat(utils.map((self.x - width / 2) / self.pz, 0, 1, 0, width) + width / 2);
-        const py: i32 = @intFromFloat(utils.map((self.y - height / 2) / self.pz, 0, 1, 0, height) + height / 2);
+        const scaleFactor = fov / (fov + self.z);
 
-        const r: f32 = utils.map(self.z, 1, width, 16, 0);
+        // const sx: i32 = @intFromFloat(utils.map((self.x - width / 2) / self.z, 0, 1, 0, width) + width / 2);
+        // const sy: i32 = @intFromFloat(utils.map((self.y - height / 2) / self.z, 0, 1, 0, height) + height / 2);
+
+        const sx: i32 = @intFromFloat(width / 2 + relativeX * scaleFactor);
+        const sy: i32 = @intFromFloat(height / 2 + relativeY * scaleFactor);
+
+        const scaleFactorP = fov / (fov + self.pz);
+
+        // const px: i32 = @intFromFloat(utils.map((self.x - width / 2) / self.pz, 0, 1, 0, width) + width / 2);
+        // const py: i32 = @intFromFloat(utils.map((self.y - height / 2) / self.pz, 0, 1, 0, height) + height / 2);
+
+        const px: i32 = @intFromFloat(width / 2 + relativeX * scaleFactorP);
+        const py: i32 = @intFromFloat(height / 2 + relativeY * scaleFactorP);
+
+        const r = utils.map(self.z, 1, width, 16, 0);
 
         rl.drawCircle(sx, sy, r, rl.Color.white);
         rl.drawLine(px, py, sx, sy, rl.Color.white);
@@ -40,7 +54,7 @@ const star = struct {
 pub fn run(comptime width: comptime_int, comptime height: comptime_int, comptime T: type, comptime config: T) anyerror!void {
     var stars: [config.stars]star = [1]star{star{}} ** config.stars;
 
-    rl.initWindow(width, height, "ChallengeGallery");
+    rl.initWindow(width, height, "Starfield");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
@@ -85,7 +99,12 @@ pub fn run(comptime width: comptime_int, comptime height: comptime_int, comptime
         rl.drawText(text_buffer[0..formatted_text.len :0], 20, 20, 20, rl.Color.gold);
 
         for (stars) |s| {
-            s.draw(width, height);
+            // Simulate camera at position (0, 0, 0) with a field of view of 100
+            const cameraX: f32 = 0;
+            const cameraY: f32 = 0;
+            const fov: f32 = 120;
+
+            s.draw(cameraX, cameraY, fov, width, height);
         }
 
         //----------------------------------------------------------------------------------
